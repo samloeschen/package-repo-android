45d1470bdc20cfa4c7a126272dde7c868c916495
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2015-04-10 04:29:34 -0400

    initial commit

---
+++ app/src/main/java/com/riffdigital/packagerepo/PackageListScreen.java
@@ -0,0 +1,42 @@
+package com.riffdigital.packagerepo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class PackageListScreen extends ActionBarActivity {
+    String[] packageTitles =
+            getApplicationContext().getResources().getStringArray(R.array.package_titles);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.package_list);
+
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_package_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
